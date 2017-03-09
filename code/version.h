bd621eafb5433d4aa8de39d694fab34151d999ef
Gordon Lack <gordon.lack@dsl.pipex.com>
2017-03-09 02:55:58 +0000

    Update to version GGR4.108.

--- code/version.h
+++ code/version.h
@@ -4,7 +4,7 @@
 #define PROGRAM_NAME "uemacs"
 #define PROGRAM_NAME_LONG "uEmacs/Pk"
 
-#define VERSION "GGR4.107"
+#define VERSION "GGR4.108"
 
 /* Print the version string. */
 void version(void);
