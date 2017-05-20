b6a3d0da72fc06e8f13699618a810be1828644c1
Gordon Lack <gordon.lack@dsl.pipex.com>
2017-05-20 16:49:36 +0100

    Changed ffputline() to work in chunks, so it doesn't call fputc() (and possibly myencrypt()) once for each character written. Tidied up the whole file to 4-column indenting.

--- code/fileio.c
+++ code/fileio.c
@@ -17,118 +17,108 @@ static int eofflag;                     /* end-of-file flag */
 /*
  * Open a file for reading.
  */
-int ffropen(char *fn)
-{
+int ffropen(char *fn) {
 #if (BSD | USG)
 #if EXPAND_TILDE
-        expand_tilde(fn);
+    expand_tilde(fn);
 #endif
 #if EXPAND_SHELL
-        expand_shell(fn);
+    expand_shell(fn);
 #endif
 #endif
-        if ((ffp = fopen(fn, "r")) == NULL)
-                return FIOFNF;
-        if (pathexpand) {       /* GGR */
+    if ((ffp = fopen(fn, "r")) == NULL) return FIOFNF;
+    if (pathexpand) {       /* GGR */
 /* If activating an inactive buffer, these may be the same and the
  * action of strcpy() is undefined for overlapping strings.
  * On a Mac it will crash...
  */
-                if (curbp->b_fname != fn) strcpy(curbp->b_fname, fn);
-        }
-        eofflag = FALSE;
-        return FIOSUC;
+        if (curbp->b_fname != fn) strcpy(curbp->b_fname, fn);
+    }
+    eofflag = FALSE;
+    return FIOSUC;
 }
 
 /*
  * Open a file for writing. Return TRUE if all is well, and FALSE on error
  * (cannot create).
  */
-int ffwopen(char *fn)
-{
+int ffwopen(char *fn) {
 #if (BSD | USG)
 #if EXPAND_TILDE
-        expand_tilde(fn);
+    expand_tilde(fn);
 #endif
 #if EXPAND_SHELL
-        expand_shell(fn);
+    expand_shell(fn);
 #endif
 #endif
-        if ((ffp = fopen(fn, "w")) == NULL) {
-                mlwrite("Cannot open file for writing");
-                return FIOERR;
-        }
-        if (pathexpand) {       /* GGR */
+    if ((ffp = fopen(fn, "w")) == NULL) {
+        mlwrite("Cannot open file for writing");
+        return FIOERR;
+    }
+    if (pathexpand) {       /* GGR */
 /* If activating an inactive buffer, these may be the same and the
  * action of strcpy() is undefined for overlapping strings.
  * On a Mac it will crash...
  */
-                if (curbp->b_fname != fn) strcpy(curbp->b_fname, fn);
-        }
-        return FIOSUC;
+        if (curbp->b_fname != fn) strcpy(curbp->b_fname, fn);
+    }
+    return FIOSUC;
 }
 
 /*
  * Close a file. Should look at the status in all systems.
  */
-int ffclose(void)
-{
-        /* free this since we do not need it anymore */
-        if (fline) {
-                free(fline);
-                fline = NULL;
-        }
-        eofflag = FALSE;
+int ffclose(void) {
+    if (fline) {        /* free this since we do not need it anymore */
+        free(fline);
+        fline = NULL;
+    }
+    eofflag = FALSE;
 
-#if     MSDOS & CTRLZ
-        fputc(26, ffp);         /* add a ^Z at the end of the file */
+#if MSDOS & CTRLZ
+    fputc(26, ffp);         /* add a ^Z at the end of the file */
 #endif
 
-#if     V7 | USG | BSD | (MSDOS & (MSC | TURBO))
-        if (fclose(ffp) != FALSE) {
-                mlwrite("Error closing file");
-                return FIOERR;
-        }
-        return FIOSUC;
+#if V7 | USG | BSD | (MSDOS & (MSC | TURBO))
+    if (fclose(ffp) != FALSE) {
+        mlwrite("Error closing file");
+        return FIOERR;
+    }
 #else
-        fclose(ffp);
-        return FIOSUC;
+    fclose(ffp);
 #endif
+    return FIOSUC;
 }
 
 /*
  * Write a line to the already opened file. The "buf" points to the buffer,
  * and the "nbuf" is its length, less the free newline. Return the status.
  * Check only at the newline.
+ * We process the input in copied chunks. We can't call fputs directly as:
+ *  a) there is no trailing NUL
+ *  b) we may have to encrypt the buffer, which is done in-place
  */
-int ffputline(char *buf, int nbuf)
-{
-        int i;
-#if     CRYPT
-        char c;                 /* character to translate */
+int ffputline(char *buf, int nbuf) {
+#define FFCHUNK 200
+    char tbuf[FFCHUNK+1];
 
-        if (cryptflag) {
-                for (i = 0; i < nbuf; ++i) {
-                        c = buf[i] & 0xff;
-                        myencrypt(&c, 1);
-                        fputc(c, ffp);
-                }
-        } else
-                for (i = 0; i < nbuf; ++i)
-                        fputc(buf[i] & 0xFF, ffp);
-#else
-        for (i = 0; i < nbuf; ++i)
-                fputc(buf[i] & 0xFF, ffp);
+    while(nbuf) {
+        int cc = (nbuf > FFCHUNK)? FFCHUNK: nbuf;
+        strncpy(tbuf, buf, cc);
+        tbuf[cc] = '\0';
+#if CRYPT
+        if (cryptflag) myencrypt(tbuf, cc);
 #endif
-
-        fputc('\n', ffp);
-
-        if (ferror(ffp)) {
-                mlwrite("Write I/O error");
-                return FIOERR;
-        }
-
-        return FIOSUC;
+        fputs(tbuf, ffp);
+        nbuf -= cc;
+        buf += cc;
+    }
+    fputc('\n', ffp);
+    if (ferror(ffp)) {
+        mlwrite("Write I/O error");
+        return FIOERR;
+    }
+    return FIOSUC;
 }
 
 /*
@@ -137,78 +127,72 @@ int ffputline(char *buf, int nbuf)
  * at the end of the file that don't have a newline present. Check for I/O
  * errors too. Return status.
  */
-int ffgetline(void)
-{
-        int c;          /* current character read */
-        int i;          /* current index into fline */
+int ffgetline(void) {
+    int c;          /* current character read */
+    int i;          /* current index into fline */
 
-        /* if we are at the end...return it */
-        if (eofflag)
-                return FIOEOF;
+/* If we are at the end...return it */
+    if (eofflag) return FIOEOF;
 
-        /* dump fline if it ended up too big */
-        if (flen > NSTRING) {
-                free(fline);
-                fline = NULL;
+/* Dump fline if it ended up too big */
+    if (flen > NSTRING) {
+        free(fline);
+        fline = NULL;
+    }
+
+/* If we don't have an fline, allocate one */
+    if (fline == NULL)
+        if ((fline = malloc(flen = NSTRING)) == NULL) return FIOMEM;
+
+/* Read the line in */
+    if (!nullflag) {
+        if (fgets(fline, NSTRING, ffp) == (char *) NULL) {      /* EOF ? */
+            i = 0;
+            c = EOF;
         }
-
-        /* if we don't have an fline, allocate one */
-        if (fline == NULL)
-                if ((fline = malloc(flen = NSTRING)) == NULL)
-                        return FIOMEM;
-
-        /* read the line in */
-        if (!nullflag) {
-                if (fgets(fline, NSTRING, ffp) == (char *) NULL) {      /* EOF ? */
-                        i = 0;
-                        c = EOF;
-                } else {
-                        i = strlen(fline);
-                        c = 0;
-                        if (i > 0) {
-                                c = fline[i - 1];
-                                i--;
-                        }
-                }
-        } else {
-                i = 0;
-                c = fgetc(ffp);
+        else {
+            i = strlen(fline);
+            c = 0;
+            if (i > 0) {
+                c = fline[i - 1];
+                i--;
+            }
         }
-        while (c != EOF && c != '\n') {
-                if (c) {
-                        fline[i++] = c;
-                        /* if it's longer, get more room */
-                        if (i >= flen) {
-                                flen += NSTRING;
-                                fline = realloc(fline, flen);
-                        }
-                }
-                c = fgetc(ffp);
+    }
+    else {
+        i = 0;
+        c = fgetc(ffp);
+    }
+    while (c != EOF && c != '\n') {
+        if (c) {
+            fline[i++] = c;
+            if (i >= flen) {    /* If it's longer, get more room */
+                flen += NSTRING;
+                fline = realloc(fline, flen);
+            }
         }
+        c = fgetc(ffp);
+    }
 
-        /* test for any errors that may have occured */
-        if (c == EOF) {
-                if (ferror(ffp)) {
-                        mlwrite("File read error");
-                        return FIOERR;
-                }
-
-                if (i != 0)
-                        eofflag = TRUE;
-                else
-                        return FIOEOF;
+/* Test for any errors that may have occured */
+    if (c == EOF) {
+        if (ferror(ffp)) {
+            mlwrite("File read error");
+            return FIOERR;
         }
+        if (i != 0) eofflag = TRUE;
+        else return FIOEOF;
+    }
 
-        /* terminate and decrypt the string */
-        fline[i] = 0;
+/* Terminate and decrypt the string */
+    fline[i] = 0;
 /* GGR - record the real size of the line */
-        ftrulen = i;
+    ftrulen = i;
 
-#if     CRYPT
-        if (cryptflag)
-                myencrypt(fline, strlen(fline));
+#if CRYPT
+    if (cryptflag) myencrypt(fline, strlen(fline));
 #endif
-        return FIOSUC;
+    return FIOSUC;
 }
 
 /*
@@ -216,34 +200,32 @@ int ffgetline(void)
  *
  * char *fname;         file to check for existance
  */
-int fexist(char *fname)
-{
-        FILE *fp;
+int fexist(char *fname) {
+    FILE *fp;
 #if (BSD | USG)
 #if EXPAND_TILDE
-        expand_tilde(fname);
+    expand_tilde(fname);
 #endif
 #if EXPAND_SHELL
-        expand_shell(fname);
+    expand_shell(fname);
 #endif
 #endif
-        /* try to open the file for reading */
-        fp = fopen(fname, "r");
 
-        /* if it fails, just return false! */
-        if (fp == NULL)
-                return FALSE;
+/* Try to open the file for reading */
+    fp = fopen(fname, "r");
 
-        /* otherwise, close it and report true */
-        fclose(fp);
-        return TRUE;
+/* If it fails, just return false! */
+    if (fp == NULL) return FALSE;
+
+/* Otherwise, close it and report true */
+    fclose(fp);
+    return TRUE;
 }
 
 #if BSD | USG
 
 #if EXPAND_SHELL
-void expand_shell(char *fn)
-{
+void expand_shell(char *fn) {
     char *ptr;
     char command[NFILEN];
     int c;
@@ -267,8 +249,7 @@ void expand_shell(char *fn)
 #if EXPAND_TILDE
 #include <stdlib.h>
 #include <pwd.h>
-void expand_tilde(char *fn)
-{
+void expand_tilde(char *fn) {
     char tilde_copy[NFILEN];
     char *p, *q;
     short i;
@@ -280,10 +261,10 @@ void expand_tilde(char *fn)
                 strcpy(tilde_copy, fn);
                 strcpy(fn , p);
                 i = 1;
-                /* special case for root! */
+/* Special case for root (i.e just "/")! */
                 if (fn[0]=='/' && fn[1]==0 && tilde_copy[1] != 0)
                     i++;
-                strcat(fn, &tilde_copy[i]);
+                strcat(fn, tilde_copy+i);
             }
         }
         else {
