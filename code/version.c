84dea6c63b4b031daf292fe3ef5946dfbd057674
Gordon Lack <gordon.lack@dsl.pipex.com>
2019-10-27 10:44:13 +0000

    Print utf8proc version as well.

--- code/version.c
+++ code/version.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "version.h"
 
+#include "utf8proc.h"
+
 void version(void) {
     printf("%s version %s\n", PROGRAM_NAME_LONG, VERSION);
+    printf(" utf8proc version %s\n", utf8proc_version());
 }
