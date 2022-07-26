4c4fc6d805103d716db112553c6f2ed0e8163f62
Gordon Lack <gordon.lack@dsl.pipex.com>
2022-07-26 12:59:22 +0100

    Add a simple test for multiple combining diacriticals.

---
+++ code/autotest/multi-ex.sh
@@ -0,0 +1,160 @@
+#!/bin/sh
+#
+
+# Simple testing for multiple combining diacriticals in one grapheme
+# In particular, one with 3, so that we push 2 into the ex part.
+
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+# Write out the testfile
+#
+prog='$1 != "--" {print substr($0, 4);}'
+
+# Need the -b because of the non-Unicode (invalid) 0xf1 bytes.
+# But Entware systems don't understand it, and work without it.
+#
+if awk -b '{}' /dev/null >/dev/null 2>&1; then
+    AWKARG=-b
+else
+    AWKARG=
+fi
+
+# Write out the test input file
+# It's written here with row and column markers.
+#
+awk $AWKARG "$prog" > autotest.tfile <<EOD
+-- 0123456789012345678901234567890123456789012345678901234567890123456789
+01 This is a very simple test.
+02 We can't get the bytes from a grapheme, but what we can do is put
+03 an x before and after the grapheme in question and check the columns
+04 they end up in.
+05
+06   xé⃢⃠x
+07
+08 That "e" is:
+09    e      (0x65)
+10    U+0301 (0xcc 0x81)
+11    U+20e2 (0xe2 0x83 0xa2)
+12    U+20e0 (0xe2 0x83 0xa0)
+EOD
+
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+# Write out the uemacs start-up file, that will run the tests
+#
+cat >uetest.rc <<'EOD'
+; Some uemacs code to run tests on testfile
+; Put test results into a specific buffer (test-reports)...
+; ...and switch to that buffer at the end.
+
+; After a search I need to check that $curcol, $curline $curchar and
+; $match  are what I expect them to be.
+;
+; -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+store-procedure report-status
+  select-buffer test-reports
+  insert-string %test-report
+  newline
+  1 select-buffer     ; Back to buffer from whence we came
+!endm
+
+set %fail 0
+set %ok 0
+
+; -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+store-procedure check-position
+;   Expects these to have been set, since this tests them all.
+; %expline      the line for the match
+; %expcol       the column for the match
+; %expchar      the expected "char" at point (R/h side) at the match
+; %expmatch     the text of the match
+;
+  !if &equ $curline %expline
+    set %test-report &cat %curtest &cat " - line OK: " $curline
+    set %ok &add %ok 1
+  !else
+    set %test-report &cat %curtest &cat " - WRONG line, got: " $curline
+    set %test-report &cat %test-report &cat " - expected: " %expline
+    set %fail &add %fail 1
+  !endif
+  execute-procedure report-status
+
+  !if &equ $curcol %expcol
+    set %test-report &cat %curtest &cat " - column OK: " $curcol
+    set %ok &add %ok 1
+  !else
+    set %test-report &cat %curtest &cat " - WRONG column, got: " $curcol
+    set %test-report &cat %test-report &cat " - expected: " %expcol
+    set %fail &add %fail 1
+  !endif
+  execute-procedure report-status
+
+  !if &equ $curchar 10
+    set %pchar "\n"
+  !else
+    set %pchar &chr $curchar
+  !endif
+  !if &equ $curchar %expchar
+    set %test-report &cat %curtest &cat " - at OK: " %pchar
+    set %test-report &cat %test-report &cat " expected: " %expchar
+    set %ok &add %ok 1
+  !else
+    set %test-report &cat %curtest &cat " - at WRONG char, got: " %pchar
+    set %fail &add %fail 1
+  !endif
+  execute-procedure report-status
+
+  !if &seq $match %expmatch
+    set %test-report &cat %curtest &cat " - matched OK: " %expmatch
+    set %ok &add %ok 1
+  !else
+    set %test-report &cat %curtest &cat " - match WRONG, got: " $match
+    set %test-report &cat %test-report &cat " expected: " %expmatch
+    set %fail &add %fail 1
+  !endif
+  execute-procedure report-status
+!endm
+
+; -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+; START running the code!
+;
+find-file autotest.tfile
+
+set %test-report "START: Multi-unicode char grapheme"
+execute-procedure report-status
+
+beginning-of-file
+add-mode Exact
+
+; -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+set %test-report "  search"
+execute-procedure report-status
+
+6 goto-line
+end-of-line
+search-reverse "x"
+  set %curtest Search1
+  set %expline 6
+  set %expcol 4
+  set %expchar &asc x
+  set %expmatch "x"
+execute-procedure check-position
+; Back 2 chars should step over the 9-byte grapheme to the other x).
+2 backward-character
+  set %curtest Search2
+  set %expline 6
+  set %expcol 2
+  set %expchar &asc x
+  set %expmatch "x"
+execute-procedure check-position
+
+
+; -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+select-buffer test-reports
+newline
+insert-string &cat &cat "END: ok: " %ok &cat " fail: " %fail
+newline
+unmark-buffer
+EOD
+
+# Run the tests...
+#
+./uemacs -c etc/uemacs.rc -x ./uetest.rc
