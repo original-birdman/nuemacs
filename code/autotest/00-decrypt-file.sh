864ba50ae086ca06894c6093b655919eb418fdcc
Gordon Lack <gordon.lack@dsl.pipex.com>
2023-09-17 22:31:25 +0100

    Update scripts for $crypt_mode additon.

--- code/autotest/00-decrypt-file.sh
+++ code/autotest/00-decrypt-file.sh
@@ -4,9 +4,11 @@
 TNAME=`basename $0 .sh`
 export TNAME
 
+rm -f FAIL-$TNAME
+
 # The original input file - so what we expect when we decrypt.
 #
-cat > Expected-Decrypt <<EOD
+cat > Expected-Decrypt <<'EOD'
 This is just a small test file to ensure that the Encrypt mode
 continues to work in the same way across uemacs changes, so as
 to ensure that any file encrypted in an older version can
@@ -16,38 +18,230 @@ Gordon Lack - 08 Jul, 2022.
 EOD
 
 # The result of encrypting that file, by-hand, using
-# ATestEncryptionString as the key
+# ATestEncryptionString as the key with $crypt_mode set to 0x3001
 #
-base64 -d > Decrypt-IN <<EOD
-gpoOZCm2LwaO52DOqUXdEo0EVN+8dQNZ5bqB5l362hizoDvTbxCiOR7unky+u4tDy56l+bVJ7adJ
-T5pP85uxixTSiE3lf1Hz80wSKOu9hEY+QuXiJe+mjm5VDMe0ooMQO1ASyqRhLGK3nXdsL+ohDdTE
-cmg7IEIhCPk/dUebZVKf17y2pZeY3Yuco2GrzKeH7I54eY3AUEZDEwwoLlZjksu8J1VUw2BmcYJt
-5oeywNHwDByY+CQXrtf8OGBz+ebuUSFVhY/12xKj3ClIsiAhXKfyKVPovRFu21VsZWadByhm8ATp
-HHUjwhTVIBK39VX9crSSZsUrIw==
+base64 -d > Decrypt-nUemacs <<'EOD'
+V2hQaG0wTXU0XTE4SFpvIDNlTWd1TSRtOnNEPyRPSFkwbDh5Zi8kW35+fmR2PGNEfEVLImp9YlB7
+X3l8MioKX0g3KDsncSFQPz9Dcnk6OlE6K3woTHJ2WHs0I3ooaEgwSWlqY2ddeCFKMTR0NlBPKCB+
+OSF7TmEuPG5tUGkKM3FMLjttWjcnd09PdC12ZGU0JzR7cHBCbCt6UTkmdmUlPylNK201NGdeYSpb
+XGBhIyQ7SzMqeTY8CkFLZGRmQk9XQSc6bV5aISEiZDFfZW50P3lcdTdaQEt3awoKd215UHZKSkBZ
+OmF1LjQ+Z10kdUtNLjI9c0IqCg==
+EOD
+
+# The result of encrypting that file, by-hand, using
+# ATestEncryptionString as the key with $crypt_mode set to 0x3002
+#
+base64 -d > Decrypt-pre-GGR4.120 <<'EOD'
+V2hQaG0wTXU0XTE4SFpvIDNlTWd1TSRtOnNEPyRPSFkwbDh5Zi8kW35+fmR2PGNEfEVLImp9YlB7
+X3l8MioKX0g3KDsncSFQPz9Dcnk6OlE6K3woTHJ2WHs0I3ooaEgwSWlqY2ddeCFKMTR0NlBPKCB+
+OSF7TmEuPG5tUGkKM3FMLjttWjcnd09PdC12ZGU0JzR7cHBCbCt6UTkmdmUlPylNK201NGdeYSpb
+XGBhIyQ7SzMqeTY8CkFLZGRmQk9XQSc6bV5aISEiZDFfZW50P3lcdTdaQEt3awoKd215UHZKSkBZ
+OmF1LjQ+Z10kdUtNLjI9c0IqCg==
+EOD
+
+# The result of encrypting that file, by-hand, using
+# ATestEncryptionString as the key with $crypt_mode set to 0x02
+#
+base64 -d > Decrypt-GGR4.120 <<'EOD'
+kn/RD7g4eh6s2jeDJY8Rg9NCYqUzriRMtWBJk/dt/hmLLbggb+RLyWVY2lSrf6s4sGSNhPNSuT/F
+kDK9V9WsQY8Cbgt0AZUL8Bm2ozLKR+CkSLqrVPCNV51a8aFgeT/X4hnALuZ5Ki/0rDsDhwUI14Ur
+uoMc0sl76rLNNuEBflJy5XxJFdq1vsOadCtd+L6RwB792sbDLem9mHRnQCj0EyPoCRLiBPfy897M
+GTQxEfn+/AAwVk8reRIFB/H6UgfmDCc3P1Of7dxD0uneQN3sDS5bd5cAGUNVok8bRNoGMkZZ2tiy
+zt1gTnWNPAt3i9ROe6RYjVOGUQ==
+EOD
+
+# The result of encrypting that file, by-hand, using
+# ATestEncryptionString as the key with $crypt_mode set to 0x01
+#
+base64 -d > Decrypt-GGR4.121 <<'EOD'
+E8KrkON5e+F3iKOoFF26ABJBHkOX2g8MN6baAwcdj8z9gbbTBixkmRVpr9Tsel2myjO72CcwWJ7j
+c1un4fum/kV6xxhfsAUdy6sNmhdvrRbFrOh6guQquEKo3lT0+H/ZmT+yAGCzFNHbaLk/vCv2I2nH
+D3wGUAn6KLCNNqSN1FY4+ob2exapZHUIfgj1QJkwIcIyzFQs6mr+mzrohTDPpVwG5yutkmL/w07f
+7zgImVIImV9y3KUsORPHjzsLBuN5jGg/GubRRvsl+NqxwB/tz7KKRwcyMh0PEJ/NHqZyVy0AIS7u
+xIOeCWwzkBljDxhUXUpHOrYx3w==
 EOD
 
 # Command to get uemacs to read in the encrypted file and write it out,
 # decrypted, to Decrypt-OUT
 #
-cat > uetest.rc <<EOD
+cat > uetest.rc <<'EOD'
+; Some uemacs code to run tests on testfile
+; Put test results into a specific buffer (test-reports)...
+; ...and switch to that buffer at the end.
+
+; After a search I need to check that $curcol, $curline $curchar and
+; $matchlen are what I expect them to be.
+;
+; -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+execute-file autotest/report-status.rc
+
+set %test_name &env TNAME
+
+select-buffer test-reports
+insert-string &cat %test_name " started"
+newline
+set %fail 0
+set %ok 0
+
+; Define a function to compare two buffers.
+; Good enough for the small ones we have here...
+; No way to pass parematers here, so we'll compare; %tbuf1 with $tbuf2
+;
+store-procedure compare-buffers
+
+  set .sbuf $cbufname
+; Set a sanity limit - just in case...
+  set .lc 100
+  set .res FAILED
+  select-buffer %tbuf1
+  beginning-of-file
+  select-buffer %tbuf2
+  beginning-of-file
+
+  !while &gre .lc 0
+    select-buffer %tbuf1
+
+    set .b1line $line
+    !force next-line
+    set .b1eof $force_status
+
+    select-buffer %tbuf2
+
+    set .b2line $line
+    !force next-line
+    set .b2eof $force_status
+
+    !if &not &seq .b1eof .b2eof
+        !break
+    !endif
+    !if &not &seq .b1line .b2line
+        !break
+    !endif
+
+    !if &seq .b1eof FAILED
+        set .res OK
+        !break
+    !endif
+
+    set .lc &sub .lc 1
+
+  !endwhile
+
+  select-buffer .sbuf
+  !if &seq .res OK
+    !return
+  !else
+    !finish
+  endif
+!endm
+
+store-procedure check-decrypt
+  !force execute-procedure compare-buffers
+  !if &seq PASSED $force_status
+    set %test-report &cat %test_name ": OK"
+    set %ok &add %ok 1
+    shell-command &cat "rm -f " %test_name
+  !else
+    set %test-report &cat %test_name ": FAILED"
+    set %fail &add %fail 1
+  !endif
+  execute-procedure report-status
+!endm
+
+; Get the expected results into buffers now...before CRYPT is on
+find-file Expected-Decrypt
+set %tbuf1 Expected-Decrypt
+;
+set %test_name Decrypt-nUemacs
+select-buffer TEST
+set $crypt_mode 0x3001
 add-mode Crypt
-read-file Decrypt-IN ATestEncryptionString
-delete-mode Crypt
-write-file Decrypt-OUT
+set-encryption-key ATestEncryptionString
+read-file %test_name
+set %tbuf2 TEST
+select-buffer test-reports
+execute-procedure check-decrypt
+;
+; We'll do this one by re-using the same buffer.
+; This will test re-encrypting the key under the new mode.
+; We have to remove the previous contents of TEST first
+;
+set %test_name Decrypt-pre-GGR4.120
+select-buffer TEST
+beginning-of-file
+set-mark
+end-of-file
+2 kill-region
+set $crypt_mode 0x3002
+unmark-buffer
+read-file %test_name
+set %tbuf2 TEST
+select-buffer test-reports
+execute-procedure check-decrypt
+;
+; Read this one into its own new buffer, giving the encryption
+; key as part of the read-file command
+;
+set %test_name Decrypt-GGR4.120
+select-buffer %test_name
+set $crypt_mode 0x2
+add-mode Crypt
+read-file %test_name ATestEncryptionString
+set %tbuf2 %test_name
+select-buffer test-reports
+execute-procedure check-decrypt
+;
+; For this one we'll set the encryption key separately
+;
+set %test_name Decrypt-GGR4.121
+select-buffer %test_name
+set $crypt_mode 0x1
+add-mode Crypt
+set-encryption-key ATestEncryptionString
+read-file %test_name
+set %tbuf2 %test_name
+select-buffer test-reports
+execute-procedure check-decrypt
+;
+select-buffer test-reports
+newline
+insert-string &cat &cat "END: ok: " %ok &cat " fail: " %fail
+newline
+insert-string &cat %test_name " ended"
+EOD
+
+# If running them all, leave - but first write out the buffer if there
+# were any failures.
+#
+if [ "$1" = FULL-RUN ]; then
+    cat >>uetest.rc <<EOD           # Allow var expansion here!
+!if &not &equ %fail 0
+    set \$cfname "FAIL-$TNAME"
+    save-file
+!else
+    unmark-buffer
+!endif
 exit-emacs
 EOD
+# Just leave display showing if being run singly.
+else
+    cat >>uetest.rc <<'EOD'
+unmark-buffer
+-2 redraw-display
+EOD
+fi
 
 # Do it...set the default uemacs if caller hasn't set one.
 [ -z "$UE2RUN" ] && UE2RUN="./uemacs -d etc"
 $UE2RUN -x ./uetest.rc
 
-# Now check that what was written out (Decrypt-OUT) is what was
-# expected (Expected-Decrypt).
-#
-if diff Decrypt-OUT Expected-Decrypt > /dev/null; then
-    echo "File decryption test passed"
-    rm -f Expected-Decrypt Decrypt-IN Decrypt-OUT uetest.rc
-else
-    echo "File decryption test FAILED"
-    echo "Expected-Decrypt Decrypt-IN Decrypt-OUT and uetest.rc left for viewing"
+if [ "$1" = FULL-RUN ]; then
+    if [ -f FAIL-$TNAME ]; then
+        echo "$TNAME FAILed"
+        echo "Files left: " *Decrypt*
+    else
+        echo "$TNAME passed"
+        rm -f Expected-Decrypt
+    fi
 fi
--- code/autotest/00-encrypt-file.sh
+++ code/autotest/00-encrypt-file.sh
@@ -4,9 +4,11 @@
 TNAME=`basename $0 .sh`
 export TNAME
 
+rm -f FAIL-$TNAME
+
 # The original input file
 #
-cat > Encrypt-IN <<EOD
+cat > Encrypt-IN <<'EOD'
 This is just a small test file to ensure that the Encrypt mode
 continues to work in the same way across uemacs changes, so as
 to ensure that any file encrypted in an older version can
@@ -15,39 +17,255 @@ still be read in the current one.
 Gordon Lack - 08 Jul, 2022.
 EOD
 
-# What we expect from  encrypting that file, by-hand, using
-# ATestEncryptionString as the key
+# The commadn we will use to check results (hence the ./)
 #
-base64 -d > Expected-Encrypt <<EOD
-gpoOZCm2LwaO52DOqUXdEo0EVN+8dQNZ5bqB5l362hizoDvTbxCiOR7unky+u4tDy56l+bVJ7adJ
-T5pP85uxixTSiE3lf1Hz80wSKOu9hEY+QuXiJe+mjm5VDMe0ooMQO1ASyqRhLGK3nXdsL+ohDdTE
-cmg7IEIhCPk/dUebZVKf17y2pZeY3Yuco2GrzKeH7I54eY3AUEZDEwwoLlZjksu8J1VUw2BmcYJt
-5oeywNHwDByY+CQXrtf8OGBz+ebuUSFVhY/12xKj3ClIsiAhXKfyKVPovRFu21VsZWadByhm8ATp
-HHUjwhTVIBK39VX9crSSZsUrIw==
+TF_NAME="./comp-file.sh"
+export TF_NAME
+cat > $TF_NAME <<'EOD'
+#/bin/sh
+#
+file="$1"
+if cmp -s "$file" "${file}-NOW"; then
+    rm -f "$file" "${file}-NOW"
+    echo PASSED
+else
+    echo FAILED     # And leave files there
+fi
+exit 0
+EOD
+chmod +x $TF_NAME
+
+# What we expect from  encrypting that file, by-hand, using
+# ATestEncryptionString as the key with $crypt_mode set to various
+# values
+#
+base64 -d > Encrypt-nUemacs <<'EOD'
+V2hQaG0wTXU0XTE4SFpvIDNlTWd1TSRtOnNEPyRPSFkwbDh5Zi8kW35+fmR2PGNEfEVLImp9YlB7
+X3l8MioKX0g3KDsncSFQPz9Dcnk6OlE6K3woTHJ2WHs0I3ooaEgwSWlqY2ddeCFKMTR0NlBPKCB+
+OSF7TmEuPG5tUGkKM3FMLjttWjcnd09PdC12ZGU0JzR7cHBCbCt6UTkmdmUlPylNK201NGdeYSpb
+XGBhIyQ7SzMqeTY8CkFLZGRmQk9XQSc6bV5aISEiZDFfZW50P3lcdTdaQEt3awoKd215UHZKSkBZ
+OmF1LjQ+Z10kdUtNLjI9c0IqCg==
 EOD
 
-# Command to get uemacs to read in the encrypted file and write it out,
-# decrypted, to Decrypted-OUT
+# The result of encrypting that file, by-hand, using
+# ATestEncryptionString as the key with $crypt_mode set to 0x3002
 #
-cat > uetest.rc <<EOD
+base64 -d > Encrypt-pre-GGR4.120 <<'EOD'
+V2hQaG0wTXU0XTE4SFpvIDNlTWd1TSRtOnNEPyRPSFkwbDh5Zi8kW35+fmR2PGNEfEVLImp9YlB7
+X3l8MioKX0g3KDsncSFQPz9Dcnk6OlE6K3woTHJ2WHs0I3ooaEgwSWlqY2ddeCFKMTR0NlBPKCB+
+OSF7TmEuPG5tUGkKM3FMLjttWjcnd09PdC12ZGU0JzR7cHBCbCt6UTkmdmUlPylNK201NGdeYSpb
+XGBhIyQ7SzMqeTY8CkFLZGRmQk9XQSc6bV5aISEiZDFfZW50P3lcdTdaQEt3awoKd215UHZKSkBZ
+OmF1LjQ+Z10kdUtNLjI9c0IqCg==
+EOD
+
+# The result of encrypting that file, by-hand, using
+# ATestEncryptionString as the key with $crypt_mode set to 0x02
+#
+base64 -d > Encrypt-GGR4.120 <<'EOD'
+kn/RD7g4eh6s2jeDJY8Rg9NCYqUzriRMtWBJk/dt/hmLLbggb+RLyWVY2lSrf6s4sGSNhPNSuT/F
+kDK9V9WsQY8Cbgt0AZUL8Bm2ozLKR+CkSLqrVPCNV51a8aFgeT/X4hnALuZ5Ki/0rDsDhwUI14Ur
+uoMc0sl76rLNNuEBflJy5XxJFdq1vsOadCtd+L6RwB792sbDLem9mHRnQCj0EyPoCRLiBPfy897M
+GTQxEfn+/AAwVk8reRIFB/H6UgfmDCc3P1Of7dxD0uneQN3sDS5bd5cAGUNVok8bRNoGMkZZ2tiy
+zt1gTnWNPAt3i9ROe6RYjVOGUQ==
+EOD
+
+# The result of encrypting that file, by-hand, using
+# ATestEncryptionString as the key with $crypt_mode set to 0x01
+#
+base64 -d > Encrypt-GGR4.121 <<'EOD'
+E8KrkON5e+F3iKOoFF26ABJBHkOX2g8MN6baAwcdj8z9gbbTBixkmRVpr9Tsel2myjO72CcwWJ7j
+c1un4fum/kV6xxhfsAUdy6sNmhdvrRbFrOh6guQquEKo3lT0+H/ZmT+yAGCzFNHbaLk/vCv2I2nH
+D3wGUAn6KLCNNqSN1FY4+ob2exapZHUIfgj1QJkwIcIyzFQs6mr+mzrohTDPpVwG5yutkmL/w07f
+7zgImVIImV9y3KUsORPHjzsLBuN5jGg/GubRRvsl+NqxwB/tz7KKRwcyMh0PEJ/NHqZyVy0AIS7u
+xIOeCWwzkBljDxhUXUpHOrYx3w==
+EOD
+
+# Command to get uemacs to read in the file and write it out
+# under various encrypted modes.
+# NEED TO FIGURE OUT HOW TO TEST OUTPUT FILES IN UEMACS
+# WHAT ABOUT ONE OF THE SPAWN COMMAND TO RUN cmp???
+#
+cat > uetest.rc <<'EOD'
+; Some uemacs code to run tests on testfile
+; Put test results into a specific buffer (test-reports)...
+; ...and switch to that buffer at the end.
+
+; After a search I need to check that $curcol, $curline $curchar and
+; $matchlen are what I expect them to be.
+;
+; -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+execute-file autotest/report-status.rc
+
+set %test_name &env TNAME
+
+select-buffer test-reports
+insert-string &cat %test_name " started"
+newline
+set %fail 0
+set %ok 0
+
+; Define a function to compare two buffers.
+; Good enough for the small ones we have here...
+; No way to pass parematers here, so we'll compare; %tbuf1 with $tbuf2
+;
+store-procedure compare-buffers
+
+  set .sbuf $cbufname
+; Set a sanity limit - just in case...
+  set .lc 100
+  set .res FAILED
+  select-buffer %tbuf1
+  beginning-of-file
+  select-buffer %tbuf2
+  beginning-of-file
+
+  !while &gre .lc 0
+    select-buffer %tbuf1
+
+    set .b1line $line
+    !force next-line
+    set .b1eof $force_status
+
+    select-buffer %tbuf2
+
+    set .b2line $line
+    !force next-line
+    set .b2eof $force_status
+
+    !if &not &seq .b1eof .b2eof
+        !break
+    !endif
+    !if &not &seq .b1line .b2line
+        !break
+    !endif
+
+    !if &seq .b1eof FAILED
+        set .res OK
+        !break
+    !endif
+
+    set .lc &sub .lc 1
+
+  !endwhile
+
+  select-buffer .sbuf
+  !if &seq .res OK
+    !return
+  !else
+    !finish
+  endif
+!endm
+
+store-procedure check-encrypt
+  select-buffer TEST
+  filter-buffer &ptf "%s %s" &env TF_NAME %test_name
+  !if &seq PASSED $line
+      set %test-report &cat %test_name ": OK"
+      set %ok &add %ok 1
+  !else
+      set %test-report &cat %test_name ": FAILED"
+      set %fail &add %fail 1
+  !endif
+  unmark-buffer
+  execute-procedure report-status
+!endm
+
+; Read in the original file
+select-buffer ORIG
 read-file Encrypt-IN
+;
+; Write this out using nUmeacs crypt mode
+set %test_name Encrypt-nUemacs
+set $crypt_mode 0x3001
 add-mode Crypt
-write-file Encrypt-OUT ATestEncryptionString
+set-encryption-key ATestEncryptionString
+write-file &cat %test_name "-NOW"
+; Run a filter to comapare what we wrote vs what we expext
+execute-procedure check-encrypt
+!force kill-buffer %test_name
+
+; Now Encrypt-pre-GGR4.120
+;
+; We'll do this one by re-using the same buffer.
+; This will test re-encrypting the key under the new mode.
+; We have to remove the previous contents of TEST first
+;
+set %test_name Encrypt-pre-GGR4.120
+select-buffer ORIG
+set $crypt_mode 0x3002
+write-file &cat %test_name "-NOW"
+execute-procedure check-encrypt
+!force kill-buffer %test_name
+
+; Read this one from a new buffer, giving the encryption
+; key as part of the read-file command
+;
+set $debug 0x02
+set %test_name Encrypt-GGR4.120
+select-buffer NOT-ORIG
+delete-buffer ORIG
+select-buffer ORIG
+read-file Encrypt-IN
+set $crypt_mode 0x2
+add-mode Crypt
+write-file &cat %test_name "-NOW" ATestEncryptionString
+execute-procedure check-encrypt
+!force kill-buffer %test_name
+set $debug 0
+
+; For this one we'll set the encryption key separately
+;
+set %test_name Encrypt-GGR4.121
+select-buffer NOT-ORIG
+delete-buffer ORIG
+select-buffer ORIG
+read-file Encrypt-IN
+set $crypt_mode 0x1
+add-mode Crypt
+set-encryption-key ATestEncryptionString
+write-file &cat %test_name "-NOW"
+execute-procedure check-encrypt
+!force kill-buffer %test_name
+
+select-buffer test-reports
+newline
+insert-string &cat &cat "END: ok: " %ok &cat " fail: " %fail
+newline
+insert-string &cat %test_name " ended"
+EOD
+
+# If running them all, leave - but first write out teh buffer if there
+# were any failures.
+#
+if [ "$1" = FULL-RUN ]; then
+    cat >>uetest.rc <<EOD           # Allow var expansion here!
+!if &not &equ %fail 0
+    set \$cfname "FAIL-$TNAME"
+    save-file
+!else
+    unmark-buffer
+!endif
 exit-emacs
 EOD
+# Just leave display showing if being run singly.
+else
+    cat >>uetest.rc <<'EOD'
+unmark-buffer
+-2 redraw-display
+EOD
+fi
 
 # Do it...set the default uemacs if caller hasn't set one.
 [ -z "$UE2RUN" ] && UE2RUN="./uemacs -d etc"
 $UE2RUN -x ./uetest.rc
 
-# Now check that what was written out (Encrypt-OUT) is what was
-# expected.
-# Binary files, so use cmp.
-#
-if cmp -s Expected-Encrypt Encrypt-OUT; then
-    echo "File encryption test passed"
-    rm -f Encrypt-IN Encrypt-OUT Expected-Encrypt uetest.rc
-else
-    echo "File encryption test FAILED"
-    echo "Encrypt-IN Encrypt-OUT Expected-Encrypt and uetest.rc left for viewing"
+if [ "$1" = FULL-RUN ]; then
+    if [ -f FAIL-$TNAME ]; then
+        echo "$TNAME FAILed"
+        echo "Files left: " *Encrypt*
+    else
+        echo "$TNAME passed"
+        rm -f Encrypt-IN
+    fi
 fi
