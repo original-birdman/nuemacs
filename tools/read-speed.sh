09c5edd29519ed437d8f6a8c32b7105f9235cbe6
Gordon Lack <gordon.lack@dsl.pipex.com>
2024-01-17 23:12:17 +0000

    Add a simple script to determine the reading speed of uemacs in lines/sec.  Very basic....

---
+++ tools/read-speed.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+
+lc=1000
+rm -f read-speed.tfile
+while [ $lc -gt 0 ]; do
+    cat ../CHANGES search.c >> read-speed.tfile
+    let "lc=$lc - 1"
+done
+
+lines=`wc -l < read-speed.tfile`
+
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+# Write out the uemacs start-up file, that will run the tests
+# This contains all of the tests
+#
+cat >uetest.rc <<'EOD'
+
+find-file read-speed.tfile
+
+exit-emacs
+EOD
+
+etime=`/usr/bin/time -f "%E" ./uemacs -x  ./uetest.rc 2>&1`
+
+rm -f read-speed.tfile
+
+(echo $etime; echo $lines) | perl -e '
+    chomp (my $etime = <>);
+    my @tbits = reverse(split ":", $etime);
+    my $time = $tbits[0];
+    shift @tbits;
+    if (@tbits) {
+        $time = $time + 60*$tbits[0];
+        shift @tbits;
+    }
+    if (@tbits) {
+        $time = $time + 60*60*$tbits[0];
+        shift @tbits;
+    }
+    chomp(my $lc = <>);
+    my $lrate = $lc/$time;
+    printf "Rate is %.2f lines/sec\n", $lrate;
+'
