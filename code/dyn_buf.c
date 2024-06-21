5ddc99a172056dec7eeb20348a6515c5f6374d58
Gordon Lack <gordon.lack@dsl.pipex.com>
2024-06-21 18:20:57 +0100

    Add BUF/STR typing to the dynamic buffer code.

--- code/dyn_buf.c
+++ code/dyn_buf.c
@@ -1,6 +1,6 @@
-/* db.c
+/* dyn_buf.c
  * Utility code to handle dynamic buffers (strings or binary data)
- * the "b" calls are for binary data (which are not NUL-terminated)
+ * All moving is done by mem* calls with specific lengths
  */
 
 #include <stdio.h>
@@ -28,56 +28,70 @@ char *_dbp_val_nc(db *ds) {
     return dbp_val(ds)? dbp_val(ds): "";
 }
 
-/* Binary data copy in - so NO NUL-termination
- * Also, does not assume current value has terminating NUL.
- */
-void _dbp_bset(db *ds, const void *mp, size_t n) {
-    if (n > dbp_max(ds)) _dbp_realloc(ds, n);
+/* Set value to the n bytes */
+
+void _dbp_setn(db *ds, const void *mp, size_t n) {
+    size_t need = n + dbp_type(ds);
+    if (need > dbp_max(ds)) _dbp_realloc(ds, need);
     memcpy(dbp_val(ds), mp, n);
     dbp_len(ds) = n;
+    if (dbp_type(ds) == STR) *(dbp_val(ds)+dbp_len(ds)) = '\0';
     return;
 }
 
-/* Clear a value.
- * Set the length to 0 and, if val is allocated, ensure byte 0 is 0.
- */
-void _dbp_clear(db *ds) {
-    dbp_len(ds) = 0;
-    if (ds->val) *(ds->val) = '\0';
-    return;
-}
-
-/* String copy in n bytes - NUL-terminate the result */
-
-void _dbp_setn(db *ds, const char *str, size_t n) {
-    size_t need = n + 1;    /* Allow for the NUL */
-    if (need > dbp_max(ds)) _dbp_realloc(ds, need);
-
-/* Copy in, set length and terminate at n chars */
-    memcpy(dbp_val(ds), str, n);
-    dbp_len(ds) = n;
-    *(dbp_val(ds)+n) = '\0';
-    return;
-}
-
-/* String copy in a string - so NUL-terminate a binary setting*/
+/* String copy in a NUL-terminated string */
 
 void _dbp_set(db *ds, const char *str) {
     return _dbp_setn(ds, str, strlen(str));
 }
 
-/* Append a string of n bytes */
+/* Insert n chars into buffer */
+
+void _dbp_insertn_at(db *ds, const void *mp, size_t n, size_t offs) {
+    int movers = dbp_len(ds) - offs;
+    if (movers < 0) return;   /* Offset too big */
+    size_t need = dbp_len(ds) + n + dbp_type(ds);
+    if (need > dbp_max(ds)) _dbp_realloc(ds, need);
+    memmove(dbp_val(ds)+offs+n, dbp_val(ds)+offs, movers);
+    memcpy(dbp_val(ds)+offs, mp, n);
+    dbp_len(ds) += n;
+    if (dbp_type(ds) == STR) *(dbp_val(ds)+dbp_len(ds)) = '\0';    
+    return;
+}
+
+/* Delete n chars from buffer */
+
+void _dbp_deleten_at(db *ds, int n, size_t offs) {
+/* Since we are deleting we must already have enough space */
+    if ((n + offs) > dbp_len(ds))  n = dbp_len(ds) - offs;
+    if (n < 0) return;
+    int movers = dbp_len(ds) - offs - n;
+    memmove(dbp_val(ds)+offs, dbp_val(ds)+offs+n, movers);
+    dbp_len(ds) -= n;
+    if (dbp_type(ds) == STR) *(dbp_val(ds)+dbp_len(ds)) = '\0';
+    return;
+}
+
+/* Clear a value.
+ * Set the length to 0 and, for STR, if val is allocated ensure byte 0 is 0.
+ */
+void _dbp_clear(db *ds) {
+    dbp_len(ds) = 0;
+    if ((ds->val) && (dbp_type(ds) == STR)) *(ds->val) = '\0';
+    return;
+}
+
+/* Append n bytes */
 
 void _dbp_appendn(db *ds, const char *str, size_t n) {
-    size_t need = dbp_len(ds) + n + 1;  /* Allow for the NUL */
+    size_t need = dbp_len(ds) + n + dbp_type(ds);
     if (need > dbp_max(ds)) _dbp_realloc(ds, need);
 
-/* Append n chars, set length and terminate at need-1 chars */
+/* Append n chars, set length and terminate if needed */
 
     memcpy(dbp_val(ds) + dbp_len(ds), str, n);
-    need--;     /* Length without NUL */
-    *(dbp_val(ds)+need) = '\0';
-    dbp_len(ds) = need;
+    dbp_len(ds) += n;
+    if (dbp_type(ds) == STR) *(dbp_val(ds)+dbp_len(ds)) = '\0';
     return;
 }
 
@@ -92,15 +106,15 @@ void _dbp_append(db *ds, const char *str) {
 /* Append a character */
 
 void _dbp_addch(db *ds, const char ch) {
-    size_t n = dbp_len(ds) + 1;
-    if (n >= dbp_max(ds)) _dbp_realloc(ds, n + 1);
+    size_t need = dbp_len(ds) + 1 + dbp_type(ds);
+    if (need > dbp_max(ds)) _dbp_realloc(ds, need);
 
 /* We know the destination length, so just drop the new char at the end. */
 
     char *eloc = dbp_val(ds) + dbp_len(ds);
     *eloc++ = ch;
-    *eloc = '\0';
-    dbp_len(ds) = n;
+    dbp_len(ds)++;
+    if (dbp_type(ds) == STR) *eloc = '\0';
     return;
 }
 
@@ -111,17 +125,21 @@ char _dbp_charat(db *ds, size_t w) {
     return *(dbp_val(ds) + w);
 }
 
-/* Set char at offset (0-based) */
+/* Set char at offset (0-based)
+ * This DOES NOT APPEND ANY NUL - it assumes it is already there if needed.
+ * It DOES set the len iff we put a NUL into a STR type.
+ */
 
 int _dbp_setcharat(db *ds, size_t w, char c) {
     if (w >= dbp_len(ds)) return 1;
     *(dbp_val(ds) + w) = c;
 /* If we've written a NUL, change the stored length */
-    if (c == '\0') dbp_len(ds) = w;
+    if ((dbp_type(ds) == STR) && (c == '\0')) dbp_len(ds) = w;
     return 0;
 }
 
 /* Compare a string.
+ * The non-n versions only make sense for STR buffers.
  * MAKE THESE #defines?
  */
 
@@ -138,7 +156,9 @@ char _dbp_casecmpn(db *ds, const char *str, size_t n) {
     return strncasecmp(dbp_val(ds), str, n);
 }
 
-/* sprintf-style call to format a db */
+/* sprintf-style call to format a db.
+ * NOTE that this always append a NUL char
+ */
 
 void _dbp_sprintf(db *ds, const char *fmt, ...) {
     va_list ap;
@@ -156,9 +176,8 @@ void _dbp_sprintf(db *ds, const char *fmt, ...) {
 /* Free (reset) a Dynamic String */
 
 void _dbp_free(db *ds) {
-    free(dbp_val(ds));
+    Xfree_setnull(dbp_val(ds));
     dbp_len(ds) = 0;
     dbp_max(ds) = 0;
-    dbp_val(ds) = NULL;
     return;
 }
--- code/dyn_buf.h
+++ code/dyn_buf.h
@@ -1,45 +1,54 @@
-/* dstr.h
- * Definitions to handle dynamic strings
+/* dyn_buf.h
+ * Definitions to handle dynamic buffers
  */
 
 #ifndef DYN_BUF_H_
 #define DYN_BUF_H_
 
-/* Define a Dynamic String, and how to access its members */
-
+/* Define a Dynamic Buffer, and how to access its members
+ * The enum values are specifically set, as it reflects the additional
+ * buffer size required beyond the valid stored bytes.
+ */
+enum db_type { BUF = 0, STR = 1 };
 typedef struct {
     char *val;      /* The (NUL-terminated) string */
     size_t len;     /* WITHOUT the NUL */
     size_t alloc;   /* What we've allocated */
+    enum db_type type;
 } db;
 
 /* We need to define these (globally, or statically in a file body)
  * So we also need to be able to declare the global ones in other
  * files.
  */
-#define db_initval { NULL, 0, 0 }
-#define db_def(a) db a = db_initval
+#define db_buf_initval { NULL, 0, 0, BUF }
+#define db_str_initval { NULL, 0, 0, STR }
+#define db_bufdef(a) db a = db_buf_initval
+#define db_strdef(a) db a = db_str_initval
 #define db_dcl(a) db a
 
 /* We need to access the entries for local/global entries (db_*)
  * and entries arriving as function parameters (dbp_*).
  */
-#define db_val(a) ((a).val)
-#define db_len(a) ((a).len)
-#define db_max(a) ((a).alloc)
-#define dbp_val(a) ((a)->val)
-#define dbp_len(a) ((a)->len)
-#define dbp_max(a) ((a)->alloc)
+#define db_val(a)   ((a).val)
+#define db_len(a)   ((a).len)
+#define db_max(a)   ((a).alloc)
+#define db_type(a)  ((a).type)
+#define dbp_val(a)  ((a)->val)
+#define dbp_len(a)  ((a)->len)
+#define dbp_max(a)  ((a)->alloc)
+#define dbp_type(a) ((a)->type)
 
 /* The actual function calls (in dyn_str.c) to manipulate them.
  * Not expecting these to be called directly
  */
 
 char *_dbp_val_nc(db *);
-void _dbp_clear(db *);
-void _dbp_setn(db *, const char *, size_t);
+void _dbp_setn(db *, const void *, size_t);
 void _dbp_set(db *, const char *);
-void _dbp_bset(db *ds, const void *, size_t);
+void _dbp_insertn_at(db *, const void *, size_t, size_t);
+void _dbp_deleten_at(db *, int, size_t);
+void _dbp_clear(db *);
 void _dbp_appendn(db *, const char *, size_t);
 void _dbp_append(db *, const char *);
 void _dbp_addch(db *, const char);
@@ -62,14 +71,19 @@ void _dbp_free(db *);
 #define db_val_nc(val) _dbp_val_nc(&(val))
 #define dbp_val_nc(val) _dbp_val_nc(val)
 
-#define db_setn(to_ds, from_ds, flen) _dbp_setn(&(to_ds), from_ds, flen)
-#define dbp_setn(to_ds, from_ds, flen) _dbp_setn((to_ds), from_ds, flen)
+#define db_setn(to_ds, from_buf, flen) _dbp_setn(&(to_ds), from_buf, flen)
+#define dbp_setn(to_ds, from_buf, flen) _dbp_setn((to_ds), from_buf, flen)
 
-#define db_set(to_ds, from_ds) _dbp_set(&(to_ds), from_ds)
-#define dbp_set(to_ds, from_ds) _dbp_set((to_ds), from_ds)
+#define db_set(to_ds, from_str) _dbp_set(&(to_ds), from_str)
+#define dbp_set(to_ds, from_str) _dbp_set((to_ds), from_str)
 
-#define db_bset(to_ds, from_ds, flen) _dbp_bset(&(to_ds), from_ds, flen)
-#define dbp_bset(to_ds, from_ds, flen) _dbp_bset((to_ds), from_ds, flen)
+#define db_insertn_at(to_ds, from_buf, flen, w) \
+     _dbp_insertn_at(&(to_ds), from_buf, flen, w)
+#define dbp_insertn_at(to_ds, from_buf, flen, w) \
+     _dbp_insertn_at((to_ds), from_buf, flen, w)
+
+#define db_deleten_at(to_ds, n, w) _dbp_deleten_at(&(to_ds), n, w);
+#define dbp_deleten_at(to_ds, n, w) _dbp_deleten_at((to_ds), n, w);
 
 #define db_clear(ds) _dbp_clear(&(ds))
 #define dbp_clear(ds) _dbp_clear((ds))
